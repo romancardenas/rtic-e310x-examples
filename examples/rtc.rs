bb67688f4cf67fffbc5e7f6d19fe7b2a5555871c
Daniel Pérez <steew0x8@protonmail.com>
2023-04-19 17:15:23 +0200

    init UART and RTC

--- examples/rtc.rs
+++ examples/rtc.rs
@@ -1,13 +1,15 @@
 #![no_main]
 #![no_std]
+#![feature(type_alias_impl_trait)]
 
 use panic_halt as _;
 use riscv_rt as _;
 
 #[rtic::app(device = e310x, dispatchers = [SoftLow])]
+
 mod app {
+    use hifive1::hal::prelude::*;
     use hifive1::sprintln;
-    use slic::Interrupt;
 
     /// HW handler for clearing RTC. When using SLIC, we must
     /// define a ClearX handler for every bypassed HW interrupt
@@ -20,7 +22,7 @@ mod app {
         sprintln!("clear RTC (rtccmp = {})", rtccmp);
         rtc.rtccmp.write(|w| w.bits(rtccmp + 65536));
         // we also pend the lowest priority SW task before the RTC SW task is automatically pended
-        riscv_slic::pend(Interrupt::SoftLow);
+        //riscv_slic::pend(slic::Interrupt::SoftLow);
     }
 
     #[shared]
@@ -33,7 +35,34 @@ mod app {
     fn init(_: init::Context) -> (Shared, Local) {
         // Pends the SoftLow interrupt but its handler won't run until *after*
         // `init` returns because interrupts are disabled
-        rtic::pend(Interrupt::SoftLow);
+        let dr;
+        unsafe {
+            dr = hifive1::hal::DeviceResources::steal();
+        }
+        let pins = dr.pins;
+        let p = dr.peripherals;
+        let clocks = hifive1::clock::configure(p.PRCI, p.AONCLK, 64.mhz().into());
+
+        // Disable watchdog
+        let wdg = p.WDOG;
+        wdg.wdogcfg.modify(|_, w| w.enalways().clear_bit());
+
+        hifive1::stdout::configure(
+            p.UART0,
+            hifive1::pin!(pins, uart0_tx),
+            hifive1::pin!(pins, uart0_rx),
+            115_200.bps(),
+            clocks,
+        );
+        sprintln!("init");
+
+        let mut rtc = p.RTC.constrain();
+        rtc.disable();
+        rtc.set_scale(0);
+        rtc.set_rtc(0);
+        rtc.set_rtccmp(10000);
+        rtc.enable();
+        //soft_low::spawn().unwrap();
         sprintln!("init");
         (Shared {}, Local {})
     }
